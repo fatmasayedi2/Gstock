35a563cbb672756a881a7381bbae3ac8460f87d9
fatmasayedi2 <sayedifatma01@gmail.com>
2025-01-12 13:20:14 +0100

    Ajout du Jenkinsfile

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,28 @@
 const mongoose = require('mongoose');
-const connectToMongo = require('./db')
-connectToMongo();
+const connectToMongo = require('./db');
+connectToMongo(); // Appel à la fonction de connexion dans db.js
 
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const port = 3001;
 
-const cors = require('cors')
-const router = require('./Routes/router')
+const cors = require('cors');
+const router = require('./Routes/router');
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
-mongoose.connect("mongodb://127.0.0.1:27017/users", { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log("Connected to MongoDB");
-    // Écoutez les requêtes sur le port spécifié
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch(error => console.error("Error connecting to MongoDB:", error));
+
+// Supprime cette ligne, car la connexion est déjà gérée par connectToMongo()
+// mongoose.connect("mongodb://127.0.0.1:27017/users", { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//     app.listen(port, () => {
+//       console.log(`Server is running on port ${port}`);
+//     });
+//   })
+//   .catch(error => console.error("Error connecting to MongoDB:", error));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
+  console.log(`Example app listening on port ${port}`);
+});
